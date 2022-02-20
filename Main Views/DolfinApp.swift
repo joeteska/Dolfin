20e57d27dbf6a9f0ea32816336293995225cf974
Josef Teska <joe.teska@gmail.com>
2022-02-20 17:36:04 -0500

    Add files via upload

---
+++ Main Views/DolfinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thesis_1ST_PrototypeApp.swift
+//  Thesis 1ST Prototype
+//
+//  Created by Josef Teska on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DolfinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
